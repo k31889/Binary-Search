d4da8763c2709b97e78d2c2e7b663767f785c2b6
Samuel Woodhams <K31889@eltham-college.org.uk>
2020-12-10 12:33:26 +0000

    Add project files.

---
+++ Binary Search/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 1, 3, 5, 6, 10 };
+            int lower = 0;
+            int upper = A.Length - 1; //Array length
+            int mid = 0;
+            int search = 3;
+            bool found = false;
+
+            while (found == false && lower <= upper)
+            {
+                mid = upper + lower / 2;
+                if (A[mid] == search)
+                {
+                    found = true;
+                    Console.WriteLine("Found");
+                }
+                if (A[mid] > search)
+                {
+                    upper = mid - 1;
+                }
+                if (A[mid] < search)
+                {
+                    lower = mid + 1;
+                }
+            }
+        }
+    }
+}
